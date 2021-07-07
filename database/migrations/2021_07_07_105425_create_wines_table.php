a1fdda4c7ab33b420ee22b8d5e5dfb93e5715c99
andrew farquharson <farquharson.a@gmail.com>
2021-07-07 12:36:04 +0100

    add wines, remove students models

--- database/migrations/2021_07_07_105425_create_wines_table.php
+++ database/migrations/2021_07_07_105425_create_wines_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateStudentsTable extends Migration
+class CreateWinesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,13 @@ class CreateStudentsTable extends Migration
      */
     public function up()
     {
-        Schema::create('students', function (Blueprint $table) {
+        Schema::create('wines', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name', 50);
+            $table->string('description');
+            $table->string('color', 10);
+            $table->string('grape_variety', 50);
+            $table->string('country', 50);
             $table->timestamps();
         });
     }
@@ -26,6 +31,6 @@ class CreateStudentsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('students');
+        Schema::dropIfExists('wines');
     }
 }
